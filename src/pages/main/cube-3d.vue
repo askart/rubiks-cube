ad3889449c8fc59e8fa4c9ca87ee6899881891b7
askart <tulebaev.askar@gmail.com>
2020-11-22 11:58:30 +0600

    add cube3d

--- src/pages/main/cube-3d.vue
+++ src/pages/main/cube-3d.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="cube-3d">
-    Cube3d
-  </div>
+  <div id="cube-3d" class="cube-3d" />
 </template>
 
 <script>
+import { init, animate } from "@/lib/cube3d";
+
 export default {
-  name: "Cube3d"
+  name: "Cube3d",
+  mounted() {
+    init("cube-3d", 500, 300);
+    animate();
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 .cube-3d
   flex 1 0 auto
-  display block
+  display flex
+  align-items center
+  justify-content center
   width 100%
   height 100%
   border 1px solid black
